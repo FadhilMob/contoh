5985103efd8d89df9e4b38d9b6cfd46941ead41f
FadhilMob <fadhilsep2002@gmail.com>
2023-05-27 20:20:49 +0700

    tes

---
+++ resources/views/h.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus optio voluptas culpa odio vel cum corrupti tempore id dolor sequi, cumque ullam sint commodi, libero accusamus praesentium sit? Beatae maiores odio incidunt quos consequatur! Quam, minus accusamus. Id a neque quas dolores maxime consequatur aspernatur. Rem, magni iste. Iure odit modi odio tenetur, veniam fuga dignissimos amet soluta ducimus cumque, perspiciatis reiciendis nam dolores! Dicta nesciunt fuga cumque. Ducimus, saepe sequi voluptate itaque soluta earum fuga. Quam laboriosam eos odit nam iure velit aliquam expedita ea dolor, beatae ipsum assumenda, consequatur obcaecati autem, amet qui? Aut accusantium nulla sunt laborum!</p>
+</body>
+</html>
