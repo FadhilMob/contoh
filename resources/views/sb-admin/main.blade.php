48c0250d23624500529a52c70c16c44c19ce2408
erinasy <erinaseviyanti02@gmail.com>
2023-06-01 16:25:36 +0700

    alhamdulilah

--- resources/views/sb-admin/main.blade.php
+++ resources/views/sb-admin/main.blade.php
@@ -88,40 +88,6 @@
             </div>
             </p>
         </div>
-        {{-- <h2>DATA RHK</h2> --}}
-    {{-- <div class="card shadow mb-4">
-        <div class="card-header py-3"> --}}
-
-            {{-- TOMBOL TAMBAH --}}
-            {{-- <a href="#" class="btn btn-primary btn-icon-split">
-                <span class="icon text-white-40">
-                     <i class="fas fa-plus"></i>
-                </span>
-                 <span class="text">Tambah RHK</span>
-            </a> --}}
-
-            {{-- ALERT SUKSES --}}
-            {{-- @if (session()->has('success'))
-                 <div class="alert alert-success" role="alert">
-                     {{ session('success') }}
-                 </div> 
-            @endif
-
-        </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" id="myTable" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Nama RHK</th>
-                            <th scope="col">Aksi</th>
-                        </tr>
-                    </thead>
-                </table>
-            </div>
-        </div>
-    </div> --}}
     </div>
 @endsection
 
