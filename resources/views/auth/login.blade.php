4d285832f77f56b2f797232306d3fb01ae141d6d
FadhilMob <fadhilsep2002@gmail.com>
2023-06-02 10:18:12 +0700

    alhamdulillah part 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,8 +8,11 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block ">
-                                <img src="https://ulasku.com/wp-content/uploads/2021/01/arti-dan-makna-lambang-kota-batu-ngalam-co.jpeg" width="500" height="400"> 
+                            <div class="col-lg-5 d-none d-lg-block">
+                                <p> <center>
+                                <img src="https://th.bing.com/th/id/R.26a4fbb5b11507db98408e71938be5f3?rik=ir2%2b4ZudtSgIPg&riu=http%3a%2f%2f1.bp.blogspot.com%2f-Xix68xvfQMo%2fUOJAT1FJmHI%2fAAAAAAAAEro%2fExw177elxwE%2fs1600%2fLogo%2bKota%2bBatu.jpg&ehk=T5aEpiKTdCdhiYfVSwse9if3TmuqAvybLNIEV80usRU%3d&risl=&pid=ImgRaw&r=0" 
+                                width="400" height="370"> 
+                            </p></center>
                             </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
@@ -27,7 +30,7 @@
                                                 id="exampleInputPassword" placeholder="Password">
                                         </div>
                                         
-                                        <a href="/dashboard" class="btn btn-primary btn-user btn-block">
+                                        <a href="/dashboard" class="btn btn-success btn-user btn-block">
                                             Login
                                         </a>
                                         <hr>
